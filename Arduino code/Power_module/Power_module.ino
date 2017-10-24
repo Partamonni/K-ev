f28be7fd3b8a35daa11d3fc75d8f4192362102d7
Partamonni <Partamonni@users.noreply.github.com>
2017-10-24 11:54:57 +0300

    First working version
    - Added configuring script and modified head comment accordingly
    - Changed DEBUG back to to #define -version, also added VERBOSE tag along with DEBUG
    - Address table formatted with full zeroes
    - wdt_disable() added to prevent still occurred wdt reset loop
    - Added a lot of debug code to be outputted to serial
    - Adress validity check fixed, checks crc byte now
    - Most annoying data bug, wrong temp byte order now corrected
    - Overtemperature now shuts power

--- Arduino code/Power_module/Power_module.ino
+++ Arduino code/Power_module/Power_module.ino
@@ -1,15 +1,20 @@
-/* K-ev Power-Module script v0.2
+/* K-ev Power-Module script v1.0
  *  
  * Written by Henri Pyoria (henri.pyoria@outlook.com)
  *  
- * Temperature-IC's must be pre-configured for 10-bit resolution (possible atleast on MAX31820)
- * and addresses must be manually defined to tempAdr table. This skips teh lengthy search for 'em.
+ * Temperature-IC addresses must be manually defined 
+ * to tempAdr table. This skips teh lengthy search for 'em.
+ * IC's must support resolution configuration! (10bit used)
  * 
  * 2s watchdog implemented after the setups ready, in case of odd failures.
  */
 
 #include <avr/wdt.h>
 #include <OneWire.h>
+
+#define DEBUG 1
+#define VERBOSE 0
+ // DO NOT USE DEBUG ON LIVE SYSTEM!!! IMPERATIVE!!!
 
 const int FAN_MAX_DUTY_CYCLE = 35; //(12/72)*256 and lowered still a bit for first tests
 const int CMD_CHAR_COUNT = 2;
@@ -22,32 +27,32 @@ const int HOST_QUERY_TIME = 5000; // ms | Change for real application
 const int TMP_COUNT = 21;
 const int BYTE = 8;
 
-const bool DEBUG = true; // DO NOT USE THIS ON LIVE SYSTEM!!! IMPERATIVE!!!
+// const bool DEBUG = true; // DO NOT USE THIS ON LIVE SYSTEM!!! IMPERATIVE!!!
 
 OneWire ds(2);
 
 byte  tempAdr[TMP_COUNT][8] =
 { {0x28, 0xBA, 0x4B, 0x41, 0x09, 0x00, 0x00, 0xAE},
   {0x28, 0xEE, 0x46, 0x41, 0x09, 0x00, 0x00, 0xB0},
-  {0},
-  {0},
-  {0},
-  {0},
-  {0},
-  {0},
-  {0},
-  {0},
-  {0},
-  {0},
-  {0},
-  {0},
-  {0},
-  {0},
-  {0},
-  {0},
-  {0},
-  {0},
-  {0}
+  {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
+  {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
+  {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
+  {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
+  {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
+  {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
+  {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
+  {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
+  {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
+  {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
+  {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
+  {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
+  {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
+  {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
+  {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
+  {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
+  {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
+  {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
+  {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
 };
 byte  data[TMP_COUNT][9];
 
@@ -79,11 +84,16 @@ void serialPrint(String);
 void serialPrint(String, bool);
 void waitFor(String);
 String enquire(String);
+#if DEBUG
+void printBits(byte);
+#endif
 
 void setup() 
 {
   DDRC |= B00010000; // pinMode(mosfetSwitch, OUTPUT);
   PORTC &= B11101111; // digitalWrite(mosfetSwitch, LOW);
+  wdt_disable();
+  wdt_reset();
   pinMode(fanSwitch, OUTPUT);
   analogWrite(fanSwitch, FAN_MAX_DUTY_CYCLE);
   pinMode(curSense, INPUT);
@@ -124,6 +134,11 @@ void setup()
   }
   
   interrupt_setup();
+
+  #if DEBUG&&VERBOSE
+  
+    serialPrint("Interrupts now in place", NL);
+  #endif
   
   if(!ds.reset())
   {
@@ -135,28 +150,41 @@ void setup()
     while(!DEBUG);
   }
 
+  // Write config command and config values to all devices
+  byte setCnf[4] = {0x4E, 0x7C, 0x0A, B00111111};
+  ds.skip();
+  ds.write_bytes(setCnf, 4);
+  ds.reset(); // reset 1-Wire
+
   while(enquire("?h") != "ok"){} 
 
   serialPrint(String(current,1), NL);
   serialPrint(String(voltage,1), NL);
 
-
+  
   
   setupSuccess = true;
   wdt_reset();
   wdt_enable(WDTO_2S);
+
+  #if DEBUG&&VERBOSE
+    serialPrint("wdt reset and enabled", NL);
+  #endif
   digitalWrite(mosfetSwitch, HIGH);
 }
 
 void loop() 
 {
   wdt_reset();
+  #if DEBUG&&VERBOSE
+    serialPrint("loop() entered", NL);
+  #endif
 
   ds.reset();
   ds.skip();
   ds.write(0x44);
   
-  ms = millis();
+  ms = millis();            // Secondary loop
   while(ms+200 >= millis()) // Do over 200ms if no extra timing is 
   {                         // given for temp sensors to do conversion    
     if(criticalCFailure)
@@ -213,8 +241,9 @@ void loop()
           hostNotResponding = false;           
           digitalWrite(mosfetSwitch, HIGH);
           serialPrint("up", NL);
-          if(DEBUG)
+          #if DEBUG
             serialPrint("Host responded, gates are now open", NL);
+          #endif
         }
         wdt_reset();
       }
@@ -227,54 +256,91 @@ void loop()
         PORTC &= B11101111; // digitalWrite(mosfetSwitch, LOW)
         hostShut = true;
         serialPrint("sh", NL);
-        if(DEBUG)
+        #if DEBUG
           serialPrint("Gate is now shut", NL);
+        #endif
       } 
     }
   }
   wdt_reset();
 
+  #if DEBUG&&VERBOSE
+    serialPrint("Secondary loop completed", NL);
+  #endif
+  
   for(int i = 0; i < TMP_COUNT; ++i)
   {
-    if(tempAdr[i]) // This checks if entry has any address in it
-    {
+    if(tempAdr[i][7] != 0x00) // This checks if entry has any address in it
+    {   
+      #if DEBUG&&VERBOSE
+        serialPrint("Entered read cycle i=");
+        serialPrint(String(i, DEC), NL);
+      #endif
       ds.reset();
-      ds.select(tempAdr[i]);    
+      ds.select(tempAdr[i]);
+      #if DEBUG&&VERBOSE
+        serialPrint("Device selected", NL);
+        serialPrint("Commanding to read out the scratchpad", NL);
+      #endif          
       ds.write(0xBE); // Request sensor to read out the scratchpad
-      for(int j = 0; i < 9; ++j) // Read all 9 bytes
+      for(int j = 0; j < 9; ++j) // Read all 9 bytes
       {
-        data[i][j] = ds.read(); 
+        data[i][j] = ds.read();
       }
+      #if DEBUG&&VERBOSE
+        for(int j = 0; j < 9; ++j)
+        {
+          if(data[i][j]<=0x0F)
+            serialPrint("0");
+          serialPrint(String(data[i][j], HEX));
+        }
+        serialPrint("\nData read", NL);
+      #endif
       if(OneWire::crc8(data[i], 8) != data[i][8])
       { // If crc is wrong, set all bits to 1
         for(int k = 0; k < 9; ++k)
           data[i][k] = byte(0xFF, HEX);
-        if(DEBUG)
-          serialPrint("CRC-check failed!", NL);
+        #if DEBUG&&VERBOSE
+          serialPrint("CRC Check failed!", NL);
+        #endif
       }
     }
   }
 
-  int rawTemp;
+  int16_t rawTemp;
   for(int i = 0; i < TMP_COUNT; ++i)
   {
+    if(tempAdr[i][7] != 0x00)
+    {
       rawTemp = 0;
-      rawTemp = (data[i][0] << BYTE) | data[i][1];
+      rawTemp = (data[i][1] << BYTE) | data[i][0];
       // ^ Reads data to temporary container
       rawTemp &= ~3;
       // ^ Sets last 2 bits to zero,
       // as 10-bit resolution doesn't use them
+      #if DEBUG&&VERBOSE
+        printBits(rawTemp);
+      #endif
       temperature[i] = 85.0*(double(rawTemp)/1360.0);
 
-      if(i==0 && temperature[i] >= (OVER_HEAT+60)
+      if(i==0 && temperature[i] >= 120)
+      { // This sensor will be in the mosfet heatsink
+        PORTC &= B11101111;
         criticalTFailure = true;
+      }
       else if(i!=0 && temperature[i] >= OVER_HEAT)
-        criticalTFailure = true;
+      {
+        PORTC &= B11101111;
+        criticalTFailure = true;        
+      }
+    }
   }
   
   wdt_reset();
   
   success = true;
+  
+  #if 0
   if(Serial.readStringUntil('\n') != "ok")
   {
     serialPrint("?h", NL);
@@ -288,12 +354,15 @@ void loop()
       success = true;
     wdt_reset();
   }
+  #endif
   
   if(!success)
   {
     PORTC &= B11101111; // digitalWrite(mosfetSwitch, LOW)
     hostNotResponding = true;
-    if(DEBUG){serialPrint("Host not reached, gates are now shut", NL);}
+    #if DEBUG&&VERBOSE
+      serialPrint("Host not reached, gates are now shut", NL);
+    #endif
   }
   else
   {
@@ -303,6 +372,7 @@ void loop()
     {
         serialPrint(String(temperature[i],1), NL);
     }
+    serialPrint("", NL);
   }
 }
 
@@ -415,14 +485,13 @@ void waitFor(String messageWanted)
     
     serialInput = Serial.readStringUntil('\n');
 
-    if(DEBUG)
-    {
+    #if DEBUG
       serialPrint("Command received: ");
       serialPrint(serialInput, NL);
       Serial.print(serialInput.length(), DEC);
       Serial.flush();
       serialPrint(" bytes", NL);
-    }
+    #endif
     
     if(serialInput == messageWanted)
     {
@@ -430,7 +499,9 @@ void waitFor(String messageWanted)
     }
     else
     {
-      if(DEBUG) {serialPrint("Answer wasnt wanted, clearing", NL);}
+      #if DEBUG
+        serialPrint("Answer wasnt wanted, clearing", NL);
+      #endif
       serialInput = "";
     }
   }
@@ -449,14 +520,13 @@ String enquire(String query)
     
     serialInput = Serial.readStringUntil('\n');
 
-    if(DEBUG)
-    {
+    #if DEBUG
       serialPrint("Command received: ");
       serialPrint(serialInput, NL);
       Serial.print(serialInput.length(), DEC);
       Serial.flush();
       serialPrint(" bytes", NL);
-    }
+    #endif
     
     if(serialInput.length() == CMD_CHAR_COUNT)
     {
@@ -464,11 +534,21 @@ String enquire(String query)
     }
     else
     {
-      if(DEBUG) {serialPrint("Answer wasnt wanted, clearing", NL);}
+      #if DEBUG
+        serialPrint("Answer wasnt wanted, clearing", NL);
+      #endif
       serialInput = "";
     }
   }
   return serialInput;
 }
-
-
+#if DEBUG
+void printBits(int data)
+{
+  String bits;
+  for(int i = 0; i < 16; ++i)
+    bits += bitRead(data, 15-i);
+    
+  Serial.println(bits);
+}
+#endif
